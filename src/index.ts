1cf338cb32c909ee23e310c6890b0aa3bc326a06
vavsab <vavsab@gmail.com>
2022-04-02 20:03:42 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,76 @@
 import * as aiven from '@pulumi/aiven';
 import * as pulumi from '@pulumi/pulumi';
-import * as azure_native from '@pulumi/azure-native';
-
+import * as random from '@pulumi/random';
 
 async function main() {
-    const CONFIG = new pulumi.Config();
+    const randomStr = new random.RandomString('random-string', {
+      length: 5,
+      special: false,
+      upper: false,
+      number: false
+    });
 
-    // const provider = new aiven.Provider('timescale', {
-    //     apiToken: CONFIG.require('timescaleToken'),
+    // Setup your aiven:apiToken
+
+    const project = new aiven.Project('project', {
+      project: pulumi.interpolate`import-test-${randomStr.result}`,
+    });
+
+    // Add payment method manually to the project. Otherwise db creation will fail 
+
+    // STAGE 1
+    // Generate stack.1.json
+    const postgres = new aiven.Pg('postgres', {
+      project: project.project,
+      serviceName: pulumi.interpolate`postgres-${randomStr.result}`,
+      plan: 'hobbyist',
+      cloudName: 'do-nyc'
+    });
+
+    // STAGE 2
+    // Export state, remove postgres from it and import it back. Comment initial postgres and uncomment this one below.
+    // Generate stack.2.json
+    // See that it contains unprotected values.
+    // randomStr.result.apply(x => {
+    //   const postgres = new aiven.Pg('postgres', {
+    //     project: project.project,
+    //     serviceName: pulumi.interpolate`postgres-${randomStr.result}`,
+    //     plan: 'hobbyist',
+    //     cloudName: 'do-nyc'
+    //   },
+    //   {
+    //     import: `import-test-${x}/postgres-${x}`
     //   });
-
-    // const integration = await aiven.getServiceIntegrationEndpoint({
-    //   project: 'mapped-timescale',
-    //   endpointName: 'datadog-timescale-db',
-    // }, {
-    //   provider: provider
     // });
 
-    // return integration.id;
 
-    const aivenVpcProject = await aiven.getProjectVpc({
-      project: 'myproject142342',
-      cloudName: 'azure-westus2',
-    });
-
-    const aivenVnetPeering = new aiven.VpcPeeringConnection(
-      'aiven-peering-connection',
-      {
-        vpcId: aivenVpcProject.id,
-        peerVpc: 'test-vnet',
-        peerCloudAccount: 'a4c0d722-5616-4a4e-a6f1-636d9794a254',
-        peerAzureTenantId: 'cca924b4-1b57-455f-97fc-8a315cb8c4ea',
-        peerResourceGroup: 'test',
-        peerAzureAppId: '916a6c3e-dc99-4434-bf54-e3d893216024',
-      }
-    );
-
-    const aivenProvider = new azure_native.Provider('aiven-provider', {
-      auxiliaryTenantIds: [aivenVnetPeering.stateInfo['to-tenant-id'] as pulumi.Output<string>]
-    });
-
-    const vnet = await azure_native.network.getVirtualNetwork({
-      virtualNetworkName: 'test-vnet',
-      resourceGroupName: 'test'
-    });
-
-    const mappedVnetPeering = new azure_native.network.VirtualNetworkPeering(
-    'aiven-network-peering',
-    {
-      allowForwardedTraffic: false,
-      allowGatewayTransit: false,
-      allowVirtualNetworkAccess: true,
-      remoteVirtualNetwork: {
-        id: aivenVnetPeering.stateInfo['to-network-id'] as pulumi.Output<string>,
-      },
-      resourceGroupName: 'test',
-      useRemoteGateways: false,
-      virtualNetworkName: vnet.name,
-      virtualNetworkPeeringName: 'aiven-network-peering',
-    },
-    {
-      provider: aivenProvider,
-    }
-  );
+    // STAGE 3
+    // Remove postgres from state again and try to import  
+    // Generate stack.3.json
+    // See that it still contains unprotected values for:
+    // pg.password
+    // pg.replicaUri
+    // pg.uri
+    // pgUserConfig.adminPassword
+    // randomStr.result.apply(x => {
+    //   const postgres = new aiven.Pg('postgres', {
+    //     project: project.project,
+    //     serviceName: pulumi.interpolate`postgres-${randomStr.result}`,
+    //     plan: 'hobbyist',
+    //     cloudName: 'do-nyc'
+    //   },
+    //   {
+    //     import: `import-test-${x}/postgres-${x}`,
+    //     additionalSecretOutputs: [
+    //       'pg.password',
+    //       'pg.replicaUri',
+    //       'pg.uri',
+    //       'pgUserConfig.adminPassword',
+    //       'servicePassword',
+    //       'serviceUri',
+    //     ],
+    //   });
+    // });
 }
 
 main();
